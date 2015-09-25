1af73e2d93cf7f31196f2a6c300c014620030cc2
lobingera <lobingera@gmail.com>
2015-09-25 14:34:51 +0200

    deps/deps.jl ignored, as been build by BinDeps

--- deps/deps.jl
+++
@@ -1,5 +0,0 @@
-macro checked_lib(libname, path)
-        (Libdl.dlopen_e(path) == C_NULL) && error("Unable to load \n\n$libname ($path)\n\nPlease re-run Pkg.build(package), and restart Julia.")
-        quote const $(esc(libname)) = $path end
-    end
-@checked_lib _jl_librsvg "librsvg-2.so.2"
