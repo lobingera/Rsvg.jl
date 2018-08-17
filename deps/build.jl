857aef8798271e23620093bf00ae2fde88c326b7
lobingera <lobingera@gmail.com>
2018-08-17 18:10:27 +0200

    Sys prefix for checks

--- deps/build.jl
+++ deps/build.jl
@@ -5,18 +5,18 @@ using Compat
 
 rsvg = library_dependency("rsvg", aliases = ["librsvg", "librsvg-2.2", "librsvg-2-2", "librsvg-2", "librsvg-2.so.2"])
 
-@static if is_linux() begin
+@static if Sys.islinux() begin
         provides(AptGet, "librsvg2-2",rsvg)
     end
 end
 
-@static if is_apple() begin
+@static if Sys.isapple() begin
         using Homebrew
         provides(Homebrew.HB, "librsvg", [rsvg], os=:Darwin)
     end
 end
 
-@static if is_windows() begin
+@static if Sys.iswindows() begin
         using WinRPM
         provides(WinRPM.RPM,"librsvg-2-2",rsvg,os = :Windows)
     end
