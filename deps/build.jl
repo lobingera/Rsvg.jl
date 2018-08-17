2d286743e3c370228d7861ee8c85aa40a9a62e62
lobingera <lobingera@gmail.com>
2018-08-17 18:22:08 +0200

    correct Compat prefix

--- deps/build.jl
+++ deps/build.jl
@@ -1,22 +1,23 @@
 using BinDeps
 using Compat
 
+
 @BinDeps.setup
 
 rsvg = library_dependency("rsvg", aliases = ["librsvg", "librsvg-2.2", "librsvg-2-2", "librsvg-2", "librsvg-2.so.2"])
 
-@static if Sys.islinux() begin
+@static if Compat.Sys.islinux() begin
         provides(AptGet, "librsvg2-2",rsvg)
     end
 end
 
-@static if Sys.isapple() begin
+@static if Compat.Sys.isapple() begin
         using Homebrew
         provides(Homebrew.HB, "librsvg", [rsvg], os=:Darwin)
     end
 end
 
-@static if Sys.iswindows() begin
+@static if Compat.Sys.iswindows() begin
         using WinRPM
         provides(WinRPM.RPM,"librsvg-2-2",rsvg,os = :Windows)
     end
